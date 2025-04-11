9146a0c1990fd84fe743e86eceed2a0ed1f0793b
okwir francis <okwirfrancis2010@gmail.com>
2025-04-11 19:09:51 +0300

    deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
     'django_otp.middleware.OTPMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,7 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -173,6 +175,7 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://groupaaits.onrender.com"
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
