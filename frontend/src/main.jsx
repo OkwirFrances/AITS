1edb1d10cb3fac71b94f5015a663dba7e7e6ba76
okwir francis <okwirfrancis2010@gmail.com>
2025-04-15 15:57:56 +0300

    build

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,10 @@
+import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
 // import reportWebVitals from './reportWebVitals';
-import './main.css';
+
 
 // Create a root with concurrent mode features
 const root = ReactDOM.createRoot(document.getElementById('root'));
